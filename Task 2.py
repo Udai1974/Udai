106360ac104eaecc19549fcb23f4785287f30e7f
Udai1974 <125871019+Udai1974@users.noreply.github.com>
2025-06-30 14:38:22 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,6 @@
+#  Task 2:  Create a Personalized Greeting
+
+First = str(input ("Enter First Name : "))
+Last = str(input("Enter Last Name : "))
+
+print("Hello" , First  +  Last , "!" " Welcome to the Python programe. ")
---
+++ Task1.py
@@ -0,0 +1,11 @@
+#  Task 1: Perform Basic Mathematical Operations
+Num1 = int(input("Please Enter First Number : "))
+Num2 = int(input("Please Enter Second Number : "))
+
+print('Addition : ', Num1+Num2)
+
+print('Substraction : ', Num1 - Num2)
+
+print('Multiplication : ', Num1 * Num2)
+
+print('Division : ', Num1 / Num2)
